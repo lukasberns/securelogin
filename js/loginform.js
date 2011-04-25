e82f538273b26023425f77a5b96c363f084c4036
lukasberns <lukas_berns15@yahoo.co.jp>
2011-04-25 23:22:42 +0200

    Add missing semicolon

--- js/loginform.js
+++ js/loginform.js
@@ -18,7 +18,7 @@ $(function() {
 				var hash1 = hex_md5(step1.salt1 + password);
 				var hash2 = hex_md5(step1.salt2 + password);
 				var resp = hex_md5(step1.challenge + hash1);
-				var session_id = step1.session_id
+				var session_id = step1.session_id;
 				
 				$.post('?step=2', { session_id: session_id, hash2: hash2, resp: resp }, function(step2) {
 					if (!step2 || isNaN(step2)) {
