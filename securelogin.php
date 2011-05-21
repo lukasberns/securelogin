1117f8d3b6e420a652fb0ba5efcd2aa325bd4199
lukasberns <lukas_berns15@yahoo.co.jp>
2011-05-21 21:40:13 +0200

    Prepend securelogin to the config file name and allow it to be in one directory higher.

--- securelogin.php
+++ securelogin.php
@@ -5,9 +5,12 @@ $securelogin_root = str_ireplace($_SERVER['DOCUMENT_ROOT'], '', $securelogin_dir
 if ($securelogin_root[0] != '/') $securelogin_root = "/$securelogin_root";
 if (substr($securelogin_root, -1) == '/') $securelogin_root = substr($securelogin_root, 0, -1);
 
-$config_file = $securelogin_dir.'/config.php';
+$config_file = $securelogin_dir.'/securelogin.config.php';
 if (!file_exists($config_file)) {
-	die('Please edit the config.sample.php file and rename it to config.php in the '.$securelogin_dir.' directory.');
+	$config_file = $securelogin_dir.'/../securelogin.config.php';
+	if (!file_exists($config_file)) {
+		die('Please edit the securelogin.config.sample.php file and rename it to securelogin.config.php in the '.$securelogin_dir.' directory. You can also put it into one directory higher.');
+	}
 }
 require($config_file);
 
