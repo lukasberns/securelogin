bbd2b9c156920f46f6b477ddf543636bf8329166
lukasberns <lukas_berns15@yahoo.co.jp>
2011-05-22 16:29:06 +0200

    Add error messages when one input is missing

--- js/loginform.js
+++ js/loginform.js
@@ -10,8 +10,16 @@ $(function() {
 	
 	form.submit(function() {
 		try {
-			if (!username_input.val()) { username_input.focus(); throw 0; }
-			if (!password_input.val()) { password_input.focus(); throw 0; }
+			if (!username_input.val()) {
+				username_input.focus();
+				error_message.html('Please enter your username.');
+				throw 0;
+			}
+			if (!password_input.val()) {
+				password_input.focus();
+				error_message.html('Please enter your password.');
+				throw 0;
+			}
 			
 			var username = username_input.val();
 			var password = password_input.val();
